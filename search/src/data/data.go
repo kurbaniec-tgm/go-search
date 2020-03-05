2bd4cb1d221dce6d6fab6ebed32cb0c9993beab3
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2020-03-05 15:44:16 +0100

    Initialized project for search command

---
+++ search/src/data/data.go
@@ -0,0 +1,5 @@
+package data
+
+func Hey() string {
+	return "hey"
+}
---
+++ search/src/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"search/src/data"
+)
+
+func main() {
+	hey := data.Hey()
+	fmt.Printf("Hey! %s", hey)
+}
