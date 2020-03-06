cf9bcea3346e8fa549e1f2d6d2fa40968f2e84ac
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2020-03-06 21:32:57 +0100

    Problem with args is only encountered in IDE, not in the terminal
    `search -b "some folder with spaces\more_folders" *` works fine in terminal.

--- search/src/opts/opts.go
+++ search/src/opts/opts.go
@@ -4,6 +4,6 @@ import "github.com/gobwas/glob"
 
 type Opts struct {
 	Base   string `short:"b" long:"base" description:"The root folder from where to look after files" required:"false" default:"./"`
-	Max    int    `short:"m" long:"max" description:"Maximum amount of matches" required:"false" default:"100"`
+	Max    int    `short:"m" long:"max" description:"Maximum amount of matches" required:"false" default:"500"`
 	Search glob.Glob
 }
